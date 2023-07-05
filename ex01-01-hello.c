305bb3aa6c7810a5bad45ab7d28951f2f442d00f
unknown <silbee08@gmail.com>
2023-07-05 23:24:25 +0900

    first commit

---
+++ ex01-01-hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//stdio = standard input-output
+//<stdio.h> 는 default -> 이게 있어야 printf가 실행됨
+
+int main(void)
+
+//main이 가장먼저 실행됨
+
+{
+    printf("Hello, World!");
+
+    return 0;
+
+// return 0; 정상종료
+
+}
